41f4050d2c74d7b58c28e9487ac2884a24bca430
amkh <ammarkhalil08@gmail.com>
2021-09-21 12:38:58 +0200

    rendering API data to the web page

--- src/client/quiz/src/App.js
+++ src/client/quiz/src/App.js
@@ -3,22 +3,54 @@ import './App.css';
 
  function App() {
 
-    const[quizzes, setQuizzes] = useState([]);
+   const [quizzes, setQuizzes] = useState(null);
+     const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-      console.log("loaded");
-      fetch("http://localhost:8080/api/quiz")
-      .then((response) => response.json())
-      .then((quiz) => {
-        console.log(quiz);
-      })
-    });
+     const fetchQuiz = async () => {
 
-  return (
-    <div className="App">
-     <h1>Hi There</h1>
-    </div>
-  );
+       if (!quizzes) {
+         fetch("http://localhost:8080/api/quiz")
+           .then((response) => response.json())
+           .then((data) => {
+             console.log(data);
+             setQuizzes(data);
+           });
+       }
+     };
+
+
+     const handleClick = (correct) => {
+       if (correct) {
+         alert("Correct Answer");
+       } else {
+         alert("Wrong Answer!! :(")
+       }
+     }
+
+     const renderAnswers = (key, answer, correct) => {
+       return <div key={key} onClick={() => console.log("Clicked")}>{answer}</div>
+     }
+
+     useEffect(fetchQuiz, [quizzes]);
+
+     if (!quizzes) {
+       return <div>loading...</div>;
+     }
+
+     const quiz = quizzes[currentIndex];
+     const count = (currentIndex + 1) + "/" + quizzes.length;
+     return (
+       <div className="App">
+         <h1>Quiz app</h1>
+         <div>
+           <div>Question {count} : {quiz.question}</div>
+           {quiz.answers ? quiz.answers.map((answer, i) => {
+
+             return renderAnswers(i, answer)
+           }) : "loading..."}
+         </div>
+       </div>
+     );
 }
 
 export default App;
--- src/main/java/com/example/quizapp/repository/QuizRepository.java
+++ src/main/java/com/example/quizapp/repository/QuizRepository.java
@@ -20,8 +20,28 @@ public class QuizRepository {
             questions1.setQuestion("When was the first launch of facebook");
             questions1.setAnswers(new String[]{"1999","2000","2003","2010"});
             questions1.setIndexOfRightAnswer(2);
-
             questions.add(questions1);
+
+            QuizQuestions questions2 = new QuizQuestions();
+            questions2.setId(++idCounter);
+            questions2.setQuestion("When was the first launch of facebook");
+            questions2.setAnswers(new String[]{"1999","2000","2003","2010"});
+            questions2.setIndexOfRightAnswer(2);
+            questions.add(questions2);
+
+            QuizQuestions questions3 = new QuizQuestions();
+            questions3.setId(++idCounter);
+            questions3.setQuestion("When was the first launch of facebook");
+            questions3.setAnswers(new String[]{"1999","2000","2003","2010"});
+            questions3.setIndexOfRightAnswer(2);
+            questions.add(questions3);
+
+            QuizQuestions questions4 = new QuizQuestions();
+            questions4.setId(++idCounter);
+            questions4.setQuestion("When was the first launch of facebook");
+            questions4.setAnswers(new String[]{"1999","2000","2003","2010"});
+            questions4.setIndexOfRightAnswer(2);
+            questions.add(questions4);
         }
         return questions;
     }
